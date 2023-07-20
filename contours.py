aa4f01168090e4b9d469d1f6aca02b204b432e2c
Suraj-agrahari <Tha075bei043@tcioe.edu.np>
2023-07-20 10:35:42 +0545

    color spaces

--- contours.py
+++ contours.py
@@ -1,13 +1,12 @@
-import cv2 as cv 
+import cv2 as cv
 import numpy as np
 
 img = cv.imread('Photos/cats.jpg')
-cv.imshow('Cats',img)
+cv.imshow('Cats', img)
 
 
-
-blank = np.zeros(img.shape[:2], dtype='uint8')
-cv.imshow('Blank',blank)
+blank = np.zeros(img.shape, dtype='uint8')
+cv.imshow('Blank', blank)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
@@ -15,23 +14,25 @@ cv.imshow('Gray', gray)
 # blur = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT)
 # cv.imshow('Blur', blur)
 
-# canny = cv.Canny(blur, 125, 175)
-# cv.imshow('Canny Edges', canny)
+canny = cv.Canny(img, 125, 175)  # used to grab canny edges
+cv.imshow('Canny Edges', canny)
 
 
-# cv.CHAIN_APPROX_NONE means we will get all the coordinate where as cv.CHAIN_APPROX_simple gives only edges end 
+# cv.CHAIN_APPROX_NONE means we will get all the coordinate where as cv.CHAIN_APPROX_simple gives only edges end
 # contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 # print(f'{len(contours)} contour(s) found!')
 
 
 ret, thresh = cv.threshold(gray, 125, 225, cv.THRESH_BINARY)
-cv.imshow('Thresh',thresh)
+cv.imshow('Thresh', thresh)
 
-contours, hierarchies = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+contours, hierarchies = cv.findContours(
+    thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+# cv.RETR_LIST is used to find the mode
 print(f'{len(contours)} contour(s) found!')
 
 
-cv.drawContours(blank, contours, -1 , (0, 0 , 255), 2)
+cv.drawContours(blank, contours, -1, (0, 0, 255), 1)
 cv.imshow('Contours Drawn', blank)
 
 cv.waitKey(0)
